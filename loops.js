680a4feca406ef82a97e2a58370b79b5eab9334a
Eunomia <eunomia.dice@gmail.com>
2024-10-02 16:48:33 +0200

    final

--- loops.js
+++ loops.js
@@ -3,18 +3,37 @@
 // EX 01 -  Use a loop to remove the 'X' and use console.log to reveal the message 
 const hiddenMessage = ["X", "X", "X", "X", "W", "X", "E",  "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "L", "X", "X", "X", "X", "X", "X", "X", "X", "X", " ","X", "X", "X", "X", "X", "X", "X", "X", "D", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", "O", "X", "X", "X", "X", "X", "X", "N", "X", "X", "X", "X", "E", "X", "X", "X", "X", "X", "X", "X", "X", "X", "X", ",", "X", " ", "X"]
 
+let index = 0;
 // Your code goes here
-while (hiddenMessage.includes("X")) {
-    let letter = hiddenMessage[0];
-    if (letter === "X")
-        hiddenMessage.shift();
-    break;
+while (index < hiddenMessage.length) {
+    
+    if ("X" === hiddenMessage[index]) {
+        hiddenMessage.splice(index, 1);
+    } else {
+        index++;
+    }
 }
 
 // EX 02 - Remove the 'X' and the even numbers
 // Hint: As this is a string you should find a way to convert into an array.
 
 const hiddenCode = "XXXXXXXXXXXXXXXXXXXXXXXXXXXCXXXXXXXOXXXXXDXXXXXXXXXXXXXXXEXXXX XXXXXXXXXXXXXXX:X0XXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX3XXXXXX2XXXXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX6XXXXXXXXXXXXXXX6XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX7XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2XXXXXXXXXX8XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX8XXXXXXXXXXX4XXXXXXXXXXXX6XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXXXXXXXXXXXXXXXX9XXXX9XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX2XXX4XXXXXXXXXXX6XXXXXXXXX2XXXXXXXX3XXXXXXXX2XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX4XXXXX"
-
 // Your code goes here
 
+hiddenArray = hiddenCode.split('');
+index = 0;
+while (index < hiddenArray.length) {
+    if ("X" === hiddenArray[index]) {
+        hiddenArray.splice(index, 1);
+    } else {
+        number = parseInt(hiddenArray[index]);
+        if (0 === number % 2) {
+            hiddenArray.splice(index, 1);
+        }
+        else {
+            index++;
+        }
+    }
+}
+
+console.log(hiddenArray.join(''));

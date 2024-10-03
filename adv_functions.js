9bb97b9681760a34f8edb68cae47f6cef5f66fe6
Eunomia <eunomia.dice@gmail.com>
2024-10-03 12:30:03 +0200

    Added adv_functions.js to execution.

---
+++ adv_functions.js
@@ -0,0 +1,20 @@
+// Given an array of names of people but mixing lower case and upper case letters, you will have to:
+// - Create a function that contains the logic to refactor those names so it converts a name like `anTHoNY` to `Anthony`.
+// - A function that accepts two parameters: an array and a callback function that is in charge of refactoring all items inside that array
+// - Return the original array but with all names properly typed
+
+function refactor_names(arrayOfNames, transforName) {
+    let refactoredNames = [];
+    for (index = 0;index < arrayOfNames.length; index++)
+        refactoredNames.push(transforName(arrayOfNames[index]));
+
+    return refactoredNames.slice();
+}
+
+const people = ['JoHn', 'ChrISTiana', 'anThoNY', 'MARia', 'jaMeS', 'MIChaEl', 'jeNNIFeR'];
+
+let refactoredNames = refactor_names(people, function(originalName) {
+    return originalName.slice(0, 1).toUpperCase() + originalName.slice(1).toLowerCase();
+})
+
+console.log(refactoredNames);

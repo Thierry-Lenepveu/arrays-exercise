6205381f8525a7b1a3a207db0946ac7584251ae5
Eunomia <eunomia.dice@gmail.com>
2024-10-03 14:59:38 +0200

    updated with coffee machine.

---
+++ coffee_machine.js
@@ -0,0 +1,41 @@
+function askItem (question, firstChoice, altChoice) {
+    let item = ""
+    do {
+        item = prompt(question);
+        if (item == null)
+            break;
+        item.toLowerCase();
+        if (item === "abort") {
+            break;
+        } 
+    } while(item !== firstChoice && item !== altChoice);
+
+    if (item == null || item === "abort") {
+        throw new Error("Process aborted.");
+    }
+    
+    return item;
+}
+
+let drink = askItem("thé ou café?", "thé", "café");
+
+let withOrWithoutSugar = askItem("avec ou sans sucre?", "avec", "sans");
+
+let milk = askItem("avec ou sans lait?", "avec", "sans") === "avec";
+
+let vegeMilk = "";
+if (milk) {
+    vegeMilk = askItem("lait vegetal?", "oui", "non") === "oui";
+}
+
+let command = drink;
+command += ", ";
+command += withOrWithoutSugar;
+command += " sucre";
+command += ", ";
+
+if (milk) {
+        command += vegeMilk ? "lait végétal" : "lait de vache";
+    }
+
+document.getElementsByTagName("body")[0].innerHTML += "<h1>"+command+"</h1>";
